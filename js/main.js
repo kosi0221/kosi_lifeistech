eb8250eaaaa513aa84e45351e343a78565f9443f
kosi0221 <koshi20050221@gmail.com>
2024-06-02 15:47:57 +0900

    initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function($){
+    $('.bg-switcher').bgSwitcher({
+        images:['img/Profile.jpg', 'img/kusunoki.jpg', 'img/europe.jpg'],
+        interval: 5000,
+        loop: true,
+    })
+});
